5faf7a56a89fefb918988cd0626024d680aa152a
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-11 10:07:07 +0600

    Add `!empty`, at least 2 words & `!number` validation
    Added three basic validation for **Name** input
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab2/task1/index.php
+++ lab2/task1/index.php
@@ -1,17 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lab 2 Task 1</title>
-  </head>
-  <body>
-    <h1>PHP Form Validation</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-      <label for="name">Name</label>
-      <input type="text" name="name" id="name" />
-      <input type="submit" value="submit" />
-    </form>
-  </body>
+<!-- ID: 20-42738-1 -->
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+</head>
+
+<body>
+
+  <?php
+  $nameErr = $emailErr = $genderErr = $websiteErr = "";
+  $name = $email = $gender = $comment = $website = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+      $nameErr = "Name is required";
+    } elseif (str_word_count($_POST["name"]) < 2) {
+      $nameErr = "Must contain at least two words";
+    } elseif (!preg_match('/^[a-z]/i', $_POST["name"])) {
+      $nameErr = "Name must start with letters";
+    } else {
+      $name = $_POST["name"];
+    }
+
+    if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = $_POST["email"];
+    }
+  }
+  ?>
+
+  <h2>PHP Form Validation</h2>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    Name: <input type="text" name="name">
+    <span class="error">* <?php echo $nameErr; ?></span>
+    <br><br>
+    E-mail: <input type="text" name="email">
+    <span class="error">* <?php echo $emailErr; ?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+  </form>
+
+</body>
+
 </html>
