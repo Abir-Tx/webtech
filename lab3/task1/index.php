c899842f7823f7bc17872d5037ad740f66bfc655
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-13 09:55:32 +0600

    Create the task 1 html form
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ lab3/task1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lab 3| Task 2</title>
+</head>
+
+<body>
+	<div class="loginBox">
+		<h2>
+			Login
+		</h2>
+
+		<label for="uname">User Name: </label>
+		<input type="text" name="uname" id="uname">
+		<br><br>
+
+
+		<label for="password">Password: </label>
+		<input type="password" name="password" id="password">
+		<br><br>
+
+
+		<input type="checkbox" name="remMe" id="remMe">
+		<label for="remMe">Remember Me</label>
+		<br><br>
+
+		<input type="Submit" value="submit">
+		<a href="#">Forgot Password</a>
+	</div>
+</body>
+
+</html>
