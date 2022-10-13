4a68c4cbec1c8a8a7792074b1a766f05446bce3a
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-13 10:19:07 +0600

    Add validation for user name
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab3/task1/index.php
+++ lab3/task1/index.php
@@ -9,27 +9,48 @@
 </head>
 
 <body>
+
+	<?PHP
+	$unameErr = "";
+	$uname = "";
+
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["uname"])) {
+			$unameErr = "Name is required";
+		} elseif (!preg_match('/^[a-zA-Z0-9 ".-_]+$/', $_POST["uname"])) {
+			$unameErr = "Can only contain alpha numeric charracters, period, dash or underscore only";
+		} elseif (str_word_count($_POST["uname"]) < 2) {
+			$unameErr = "Must contain at least two words";
+		} else {
+			$uname = $_POST["uname"];
+		}
+	}
+	?>
 	<div class="loginBox">
 		<h2>
 			Login
 		</h2>
 
-		<label for="uname">User Name: </label>
-		<input type="text" name="uname" id="uname">
-		<br><br>
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+			<label for="uname">User Name: </label>
+			<input type="text" name="uname" id="uname">
+			<span class="error">* <?php echo $unameErr; ?></span>
+			<br><br>
 
 
-		<label for="password">Password: </label>
-		<input type="password" name="password" id="password">
-		<br><br>
+			<label for="password">Password: </label>
+			<input type="password" name="password" id="password">
+			<br><br>
 
 
-		<input type="checkbox" name="remMe" id="remMe">
-		<label for="remMe">Remember Me</label>
-		<br><br>
+			<input type="checkbox" name="remMe" id="remMe">
+			<label for="remMe">Remember Me</label>
+			<br><br>
 
-		<input type="Submit" value="submit">
-		<a href="#">Forgot Password</a>
+			<input type="Submit" value="submit">
+			<a href="#">Forgot Password</a>
+		</form>
 	</div>
 </body>
 
