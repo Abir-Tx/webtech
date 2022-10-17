5e539d39ae10b48b493125914196228b01c1f7e1
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-18 01:59:03 +0600

    [TEST] Convert form data to json
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab3/task1/registration.php
+++ lab3/task1/registration.php
@@ -9,27 +9,57 @@
 </head>
 
 <body>
-	<?php
-	$imageFileType = "";
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		// check size limit
-		if (
-			$imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		) {
-			echo "Sorry, only JPG, JPEG, PNG files are allowed.";
-		} elseif ($_FILES["upFile"]["size"] > 50000) {
-			echo "File size can not be over 5MB";
+	<!-- Number 4 -->
+	<div class="reg">
+		<?php
+		$nameErr = $emailErr = $dobErr =  $genderErr = $degreeErr = $bloodErr = "";
+		$name = $email = $dob = $gender = $comment = $degree = $blood = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if (empty($_POST["name"])) {
+				$nameErr = "Name is required";
+			} elseif (str_word_count($_POST["name"]) < 2) {
+				$nameErr = "Must contain at least two words";
+			} elseif (!preg_match('/^[a-z]/i', $_POST["name"])) {
+				$nameErr = "Name must start with letters";
+			} elseif (!preg_match('/^[a-zA-Z ".-]+$/', $_POST["name"])) {
+				$nameErr = "Can contain a-z, A-Z, period, dash only";
+			} else {
+				$name = $_POST["name"];
+			}
+
+			if (empty($_POST["email"])) {
+				$emailErr = "Email is required";
+			} elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "Invalid email format";
+			} else {
+				$email = $_POST["email"];
+			}
+
+			$data = array();
+			$data = array(
+				'name' => $name,
+				'email' => $email,
+			);
+			$jsonData = json_encode($data);
+
+
+			echo $jsonData;
 		}
-	}
-	?>
-	<div class="proPic">
-		<h2>Profile Picture</h2>
+		?>
+		<h2>Registration</h2>
 
-		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
-
-			<input type="file" name="upFile" id="upFile">
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+			<label for="name">Name: </label>
+			<input type="text" name="name" id="name">
+			<span class="error">* <?php echo $nameErr; ?></span>
 			<hr>
-			<input type="submit" value="Submit" name="submit">
+
+			<label for="email">Email: </label>
+			<input type="email" name="email" id="email">
+			<span class="error">* <?php echo $nameErr; ?></span>
+			<hr>
+
+			<input type="submit" value="Submit">
 		</form>
 	</div>
 </body>
