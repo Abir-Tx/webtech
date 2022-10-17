10dedf56ebf93a6a7d46193ab70769e19980e2fc
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-18 00:54:46 +0600

    Add forget password validity
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab3/task1/changePass.php
+++ lab3/task1/changePass.php
@@ -10,25 +10,51 @@
 
 <body>
 	<?php
-	$currErr = $newPassErr = $conPassErr = "";
+	$currPassErr = $newPassErr = $conPassErr = "";
+	$currPass = "abir1298@";
+	$newPass = $conPass = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["currPass"])) {
+			$currPassErr = "Current Password is required";
+		} elseif ($_POST["currPass"] != $currPass) {
+			$currPassErr = "Password does not match";
+		} else {
+			if (empty($_POST["newPass"]) || empty($_POST["conPass"])) {
+				$newPassErr = $conPassErr = "This field can not be empty";
+			}
+
+			if ($_POST["newPass"] === $_POST["conPass"]) {
+				if ($_POST["newPass"] === $currPass) {
+					$newPassErr = "The new password can not be same as current password";
+				} else {
+					$newPass = $_POST["newPass"];
+					echo "Password Updated Successfully";
+				}
+			} else $newPassErr = "The new password do not match with the retyped password";
+		}
+	}
+
 	?>
 	<h2>Change Password</h2>
 
 	<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 		<label for="currPass">Current Password: </label>
 		<input type="password" name="currPass" id="currPass">
-		<span class="error">* <?php echo $currErr; ?></span>
+		<span class="error">* <?php echo $currPassErr; ?></span>
 		<br><br>
 
-		<label for="newPass">New Password</label>
+		<label for="newPass">New Password: </label>
 		<input type="password" name="newPass" id="newPass">
 		<span class="error">* <?php echo $newPassErr; ?></span>
 		<br><br>
 
-		<label for="conPass">Confirm Password</label>
+		<label for="conPass">Retype New Password: </label>
 		<input type="password" name="conPass" id="conPass">
 		<span class="error">* <?php echo $conPassErr; ?></span>
 		<br><br>
+
+		<input type="submit" value="submit">
 	</form>
 
 </body>
