42db1a7fbfa7ca5b0ee847a8c6b1d8bafa7955b4
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-20 02:00:36 +0600

    Create a common header file
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ lab4/recruiterx/pages/header.php
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<h1>RecruiterX</h1>
+
+		<ul>
+			<li>
+				<a href="/webtech/lab4/recruiterx/index.php">Home</a>
+			</li>
+
+			<li>
+				<a href="http://localhost/webtech/lab4/recruiterx/pages/login.php">Login</a>
+			</li>
+		</ul>
+	</nav>
+</header>
