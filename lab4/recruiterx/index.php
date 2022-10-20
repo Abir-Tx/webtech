0697a388ffaac50eeecb3d5a65d756135c7d01bc
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-20 11:18:46 +0600

    Add stylings for header
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/index.php
+++ lab4/recruiterx/index.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home | Recruiter</title>
+	<link rel="stylesheet" href="./styles/scss/header.css">
 </head>
 
 <body>
--- lab4/recruiterx/pages/passReset.php
+++ lab4/recruiterx/pages/passReset.php
@@ -7,6 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Reset Password | RecruiterX</title>
+	<link rel="stylesheet" href="../styles/scss/header.css">
 </head>
 
 <body>
