a9281dc6b27447c60134926e6b602b4401e70315
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-20 11:50:00 +0600

    [TEST] Add styles for the reg page
    - Added css to make the inputs appear in the middle
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/pages/registration.php
+++ lab4/recruiterx/pages/registration.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Registration | RecruiterX</title>
 	<link rel="stylesheet" href="../styles/scss/header.css">
+	<link rel="stylesheet" href="../styles/scss/registration.css">
 </head>
 
 <body>
