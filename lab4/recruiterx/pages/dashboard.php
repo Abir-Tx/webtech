2564c9e85afe1917b7db9d9c937ec0cf6812f3f8
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-21 12:39:17 +0600

    Handle unauthorized access to dashboard page
    - if session exixt only then the user details will be shown else it will ask to login
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/pages/dashboard.php
+++ lab4/recruiterx/pages/dashboard.php
@@ -13,21 +13,29 @@
 	<?php @include "./header.php" ?>
 	<?php
 	session_start();
-	$dataFileLoc = "../data.json";
-	$data = json_decode(file_get_contents($dataFileLoc));
-	$name =  $email = "";
+	if (isset($_SESSION['uname'])) {
+		$dataFileLoc = "../data.json";
+		$data = json_decode(file_get_contents($dataFileLoc));
+		$name =  $email = "";
 
-	foreach ($data as $key => $obj) {
-		// Fetch information of only the logged in user using session
-		foreach ($obj as $item => $val) {
-			if ($_SESSION['uname'] == $val) {
-				$name = $obj->name;
-				$email = $obj->email;
+		foreach ($data as $key => $obj) {
+			// Fetch information of only the logged in user using session
+			foreach ($obj as $item => $val) {
+				if ($_SESSION['uname'] == $val) {
+					$name = $obj->name;
+					$email = $obj->email;
+				}
 			}
 		}
-	}
 
-	echo "<H1>Welcome " . ucwords($name) . "</H1>";
+		// Displaying the details
+		echo "<code>logged in as " . $_SESSION['uname'] . " | </code>";
+		print('<a href="./logout.php">logout</a>');
+		echo "<H1>Welcome " . ucwords($name) . "</H1>";
+	} else {
+		echo "<p> You need to be logged in to be able to view this page</p>";
+		echo "<p>Please Login</p>";
+	}
 	?>
 
 	<?php @include "./footer.php" ?>
