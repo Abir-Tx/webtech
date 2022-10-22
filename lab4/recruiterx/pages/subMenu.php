5c21bc880ebec29efb65e13b3f6158d4ffd7077f
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-22 13:24:48 +0600

    Copy the `changePass` file from lab3
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/pages/subMenu.php
+++ lab4/recruiterx/pages/subMenu.php
@@ -8,7 +8,7 @@
 		<li><a href="./view.php">View Profile</a></li>
 		<li><a href="./edit.php">Edit Profile</a></li>
 		<li><a href="#">Change Profile Picture</a></li>
-		<li><a href="#">Change Password</a></li>
-		<li><a href="#">Logout</a></li>
+		<li><a href="./changePass.php">Change Password</a></li>
+		<li><a href="./logout.php">Logout</a></li>
 	</ul>
 </div>
