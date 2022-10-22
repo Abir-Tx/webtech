72062ecc884457640edf051162cf116534c6aa64
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-23 00:38:38 +0600

    Add styles for the **Password Reset** page
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/pages/passReset.php
+++ lab4/recruiterx/pages/passReset.php
@@ -8,6 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Reset Password | RecruiterX</title>
 	<link rel="stylesheet" href="../styles/scss/header.css">
+	<link rel="stylesheet" href="../styles/scss/common.css">
 </head>
 
 <body>
