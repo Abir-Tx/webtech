4bbc54e3a3c3fc8dbf72c9b593ead1a51dc4344e
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-22 14:13:04 +0600

    Add a file upload option to reg page
    - Created a file upload option
    
    ref: https://www.w3schools.com/php/php_file_upload.asp
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab4/recruiterx/pages/registration.php
+++ lab4/recruiterx/pages/registration.php
@@ -78,6 +78,56 @@
 			$passed = true;
 		}
 
+		$target_dir = "../images/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+		// Check if image file is a actual image or fake image
+		if (isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if ($check !== false) {
+				echo "File is an image - " . $check["mime"] . ".";
+				$uploadOk = 1;
+			} else {
+				echo "File is not an image.";
+				$uploadOk = 0;
+			}
+		}
+
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+
+		// Check file size
+		/* if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+} */
+
+		// Allow certain file formats
+		if (
+			$imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif"
+		) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+		} else {
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
+		}
+
 
 		if ($passed) {
 			$currentData = file_get_contents($dataFileLoc);
@@ -107,7 +157,7 @@
 
 	<!-- HTML Codes -->
 	<div class="reg">
-		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
 			<div class="inputsCon">
 				<label for="name">Name: </label>
 				<input type="text" name="name" id="name">
@@ -146,6 +196,11 @@
 			<label for="date" class="inpLabel">Date of Birth: </label>
 			<input type="date" name="date" id="date">
 			<span class="error">* <?php echo $dobErr; ?></span>
+			<hr>
+
+			Select image to upload:
+			<input type="file" name="fileToUpload" id="fileToUpload">
+			<input type="submit" value="Upload Image" name="submit">
 			<hr>
 
 
