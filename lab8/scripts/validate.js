1bb628fc083b3d9427aed3f56517a3dc8b66b57e
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-11-22 10:17:06 +0600

    Add validation for country dob and edu
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab8/scripts/validate.js
+++ lab8/scripts/validate.js
@@ -7,16 +7,20 @@ function validateForm() {
   var email = document.forms["myForm"]["email"].value;
   var phone = document.forms["myForm"]["phone"].value;
   var address = document.forms["myForm"]["address"].value;
-
+  var dob = document.forms["myForm"]["dob"].value;
+  var country = document.forms["myForm"]["country"].value;
   // Name validation
   if (name == "") {
     document.getElementById("nameErr").innerHTML = "Name is required";
+    return false;
   } else if (name.length < 3) {
     document.getElementById("nameErr").innerHTML =
       "Name must be at least 3 characters";
+    return false;
   } else if (name.split(" ").length < 2) {
     document.getElementById("nameErr").innerHTML =
       "Name must be at least 2 words";
+    return false;
   } else if (
     name.match(/^[0-9]/) ||
     name.match(/^[^a-zA-Z]/) ||
@@ -26,13 +30,16 @@ function validateForm() {
   ) {
     document.getElementById("nameErr").innerHTML =
       "Name must start with a letter and can only contain letters, numbers, periods, dashes or underscores";
+    return false;
   }
 
   //   email validation
   if (email == "") {
     document.getElementById("emailErr").innerHTML = "Email is required";
+    return false;
   } else if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
     document.getElementById("emailErr").innerHTML = "Email is invalid";
+    return false;
   }
 
   //   username validation
@@ -81,4 +88,82 @@ function validateForm() {
   }
 
   //   phone validation
+  //   if +880 is present in phone then truncate it
+  if (phone.match(/^\+880/)) {
+    phone = phone.substring(3);
+  }
+
+  //   Other validations
+  if (phone == "") {
+    document.getElementById("phoneErr").innerHTML = "Phone is required";
+  }
+  //   if phone is not numeric then show error
+  else if (!phone.match(/^[0-9]+$/)) {
+    document.getElementById("phoneErr").innerHTML = "Phone must be numeric";
+  }
+  //   if phone is less than 11 characters then show error
+  else if (phone.length < 11) {
+    document.getElementById("phoneErr").innerHTML =
+      "Phone must be at least 11 characters";
+  }
+  //   if phone is greater than 14 characters then show error
+  else if (phone.length > 12) {
+    document.getElementById("phoneErr").innerHTML =
+      "Phone must be less than 12 characters";
+  }
+
+  //   address validation
+  if (address == "") {
+    document.getElementById("addressErr").innerHTML = "Address is required";
+  }
+  //   if address is less than 10 characters then show error
+  else if (address.length < 10) {
+    document.getElementById("addressErr").innerHTML =
+      "Address must be at least 10 characters";
+  }
+  //   if address is greater than 100 characters then show error
+  else if (address.length > 100) {
+    document.getElementById("addressErr").innerHTML =
+      "Address must be less than 100 characters";
+  }
+
+  // validate dob
+  if (dob == "") {
+    document.getElementById("dobErr").innerHTML = "Date of birth is required";
+  } else if (dob.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) {
+    var today = new Date();
+    var birthDate = new Date(dob);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    if (age < 18) {
+      document.getElementById("dobErr").innerHTML =
+        "You must be at least 18 years old";
+    }
+  }
+
+  // validate Education
+  if (document.getElementById("ssc").checked == false) {
+    document.getElementById("educationErr").innerHTML =
+      "At least one education is required";
+  }
+  if (document.getElementById("hsc").checked == false) {
+    document.getElementById("educationErr").innerHTML =
+      "At least one education is required";
+  }
+  if (document.getElementById("bsc").checked == false) {
+    document.getElementById("educationErr").innerHTML =
+      "At least one education is required";
+  }
+  if (document.getElementById("msc").checked == false) {
+    document.getElementById("educationErr").innerHTML =
+      "At least one education is required";
+  }
+
+  // validate Country
+  if (country == "none") {
+    document.getElementById("countryErr").innerHTML = "Country is required";
+  }
 }
