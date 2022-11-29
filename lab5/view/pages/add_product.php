af5f968f40391cfe7e982113308a7b2ed5f8c413
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-11-29 10:33:04 +0600

    Add styling to the add_product page
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- lab5/view/pages/add_product.php
+++ lab5/view/pages/add_product.php
@@ -7,27 +7,30 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Add Product</title>
+	<link rel="stylesheet" href="../scss/add.css">
 </head>
 
 <body>
 	<h2>
 		Add Product
 	</h2>
-	<form action="../../controller/create_product.php" method="POST">
-		<label for="product_name">Name</label>
-		<input type="text" name="product_name" id="product_name" required>
-		<br>
-		<label for="buying_price">Buying Price</label>
-		<input type="number" name="buying_price" id="buying_price" required>
-		<br>
-		<label for="selling_price">Selling Price</label>
-		<input type="number" name="selling_price" id="selling_price" required>
-		<br>
-		<input type="checkbox" name="display" id="display">
-		<label for="display">Display</label>
-		<br>
-		<input type="submit" value="Save">
-	</form>
+	<div class="formCon">
+		<form action="../../controller/create_product.php" method="POST">
+			<label for="product_name">Name</label>
+			<input type="text" name="product_name" id="product_name" required>
+			<br>
+			<label for="buying_price">Buying Price</label>
+			<input type="number" name="buying_price" id="buying_price" required>
+			<br>
+			<label for="selling_price">Selling Price</label>
+			<input type="number" name="selling_price" id="selling_price" required>
+			<br>
+			<input type="checkbox" name="display" id="display">
+			<label for="display">Display</label>
+			<br>
+			<input type="submit" value="Save">
+		</form>
+	</div>
 
 	<a href="../index.php">Go Homepage</a>
 </body>
