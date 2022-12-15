141a5a3fb35cac78c65a002ae65b749973e45dd5
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-12-15 15:59:44 +0600

    Create the search box
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ final_assignment/implementation_1/index.php
@@ -0,0 +1,24 @@
+<!-- 
+Code by Mushfiqur Rahman Abir. ID: 20-42738-1
+Ref: https://phppot.com/jquery/jquery-ajax-autocomplete-country-example/
+-->
+<html>
+
+<head>
+  <title>Implementation 1</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://code.jquery.com/jquery-3.6.2.min.js" integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA=" crossorigin="anonymous"></script>
+  <script src="./script.js"></script>
+</head>
+
+<!-- Body Code -->
+
+<body>
+  <h1>Implementation 1 - AJAX Autocomplete</h1>
+  <div class="frmSearch">
+    <input type="text" id="search-box" placeholder="Country Name" />
+    <div id="suggesstion-box"></div>
+  </div>
+</body>
+
+</html>
