e86f104582e76dced6aec69fdab29e1f07c6a226
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-12-15 16:01:13 +0600

    Add the AJAX script
    - Complete the implementation 1
    
    Ref: https://phppot.com/jquery/jquery-ajax-autocomplete-country-example/
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ final_assignment/implementation_1/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#search-box").keyup(function () {
+    $.ajax({
+      type: "POST",
+      url: "readCountry.php",
+      data: "keyword=" + $(this).val(),
+      beforeSend: function () {
+        $("#search-box").css(
+          "background",
+          "#FFF url(LoaderIcon.gif) no-repeat 165px"
+        );
+      },
+      success: function (data) {
+        $("#suggesstion-box").show();
+        $("#suggesstion-box").html(data);
+        $("#search-box").css("background", "#FFF");
+      },
+    });
+  });
+});
+
+function selectCountry(val) {
+  $("#search-box").val(val);
+  $("#suggesstion-box").hide();
+}
